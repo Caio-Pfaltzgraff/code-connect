9af82d4dfb9a867cda93ade16a9a75370f663d35
Caio Pfaltzgraff <caio.rllcr@gmail.com>
2024-06-13 17:44:05 -0300

    Página com parâmetros

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,10 @@
 import { CardPost } from "@/components/CardPost";
 import logger from "@/logger";
+import Link from "next/link";
 import styles from "./page.module.css";
 
-async function getAllPosts() {
-  const response = await fetch('http://localhost:3042/posts')
+async function getAllPosts(page) {
+  const response = await fetch(`http://localhost:3042/posts?_page=${page}&_per_page=6`)
   if(!response.ok) {
     logger.error('Ops, alguma coisa correu mal')
     return []
@@ -13,10 +14,12 @@ async function getAllPosts() {
 }
 
 export default async function Home() {
-  const posts = await getAllPosts();
+  const { data: posts, prev, next } = await getAllPosts(1);
   return (
     <main className={styles.grid}>
       {posts.map(post => <CardPost post={post} key={post.id}/>)}
+      {prev && <Link href={`/?page=${prev}`}>Página anterior</Link>}
+      {next && <Link href={`/?page=${next}`}>Próxima página</Link>}
     </main>
   );
 }
