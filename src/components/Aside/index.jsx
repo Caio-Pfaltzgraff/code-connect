704c6b338454f8ce5beb98b9977af7c96fddd55b
Caio Pfaltzgraff <caio.rllcr@gmail.com>
2024-06-14 15:29:39 -0300

    feat finalizando estilos do aside

--- src/components/Aside/index.jsx
+++ src/components/Aside/index.jsx
@@ -2,11 +2,53 @@ import Image from 'next/image'
 import styles from './aside.module.css'
 import logo from './logo.png'
 
+import Link from 'next/link'
+import aboutusIcon from './aboutus.png'
+import exitIcon from './exit.png'
+import feedIcon from './feed.png'
+import profileIcon from './profile.png'
+
+const links = [
+  {
+    name: 'Feed',
+    icon: feedIcon
+  },
+  {
+    name: 'Perfil',
+    icon: profileIcon
+  },
+  {
+    name: 'Sobre nós',
+    icon: aboutusIcon
+  },
+  {
+    name: 'Sair',
+    icon: exitIcon
+  },
+]
+
 export const Aside = () => {
   return (
     <aside className={styles.aside}>
-      {/* <img src="/logo.png" alt="Logo da Code Connect" /> */}
-      <Image src={logo} alt="Logo da Code Connect"/>
+      <Link href="/">
+        <Image src={logo} alt="Logo da Code Connect"/>
+      </Link>
+      <ul className={styles.items}>
+        <li>
+          <button>Publicar</button>
+        </li>
+        {links.map(l => (
+          <li key={l.name}>
+            <Image
+              src={l.icon} 
+              alt={`Icone ${l.name}`}
+              width={32}
+              height={32}
+            />
+            <p>{l.name}</p>
+          </li>
+        ))}
+      </ul>
     </aside>
   )
 }
