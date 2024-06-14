dc10249a33ceb21e0558747035127729be0894bd
Caio Pfaltzgraff <caio.rllcr@gmail.com>
2024-06-14 11:27:12 -0300

    Obtendo post de forma assíncrona

--- src/app/posts/[slug]/page.js
+++ src/app/posts/[slug]/page.js
@@ -1,5 +1,27 @@
-const PagePost = () => {
-  return <h1>Olá posts</h1>
+import logger from "@/logger";
+
+async function getPostBySlug(slug) {
+  const url = `http://localhost:3042/posts?slug=${slug}`;
+  const response = await fetch(url);
+
+  if(!response.ok) {
+    logger.error('Ops, alguma coisa correu mal')
+    return {}
+  }
+
+  logger.info('Post obtido com sucesso!')
+  const data = await response.json()
+  if(data.length == 0) {
+    return {}
+  }
+
+  return data[0];
+}
+
+const PagePost = async ({ params }) => {
+  const slug = params.slug
+  const post = await getPostBySlug(slug)
+  return <h1 style={{color: 'white'}}>{post.title}</h1>
 }
 
 export default PagePost
