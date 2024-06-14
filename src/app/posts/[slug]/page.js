1ecc132e9cd440f7a8dcefe1aa7a1158ecb84f9b
Caio Pfaltzgraff <caio.rllcr@gmail.com>
2024-06-14 12:04:51 -0300

    feat: finalizando página de posts

--- src/app/posts/[slug]/page.js
+++ src/app/posts/[slug]/page.js
@@ -1,27 +1,46 @@
+import { CardPost } from "@/components/CardPost";
 import logger from "@/logger";
+import { remark } from "remark";
+import html from "remark-html";
+import styles from "./page.module.css";
 
 async function getPostBySlug(slug) {
   const url = `http://localhost:3042/posts?slug=${slug}`;
   const response = await fetch(url);
 
-  if(!response.ok) {
-    logger.error('Ops, alguma coisa correu mal')
-    return {}
+  if (!response.ok) {
+    logger.error("Ops, alguma coisa correu mal");
+    return {};
   }
 
-  logger.info('Post obtido com sucesso!')
-  const data = await response.json()
-  if(data.length == 0) {
-    return {}
+  logger.info("Post obtido com sucesso!");
+  const data = await response.json();
+  if (data.length == 0) {
+    return {};
   }
 
-  return data[0];
+  const post = data[0];
+
+  const processedContent = await remark().use(html).process(post.markdown);
+  const contentHtml = processedContent.toString();
+
+  post.markdown = contentHtml;
+
+  return post;
 }
 
 const PagePost = async ({ params }) => {
-  const slug = params.slug
-  const post = await getPostBySlug(slug)
-  return <h1 style={{color: 'white'}}>{post.title}</h1>
-}
+  const slug = params.slug;
+  const post = await getPostBySlug(slug);
+  return (
+    <div>
+      <CardPost post={post} highlight />
+      <h3 className={styles.subtitle}>Código:</h3>
+      <div className={styles.code}>
+        <div dangerouslySetInnerHTML={{ __html: post.markdown }} />
+      </div>
+    </div>
+  );
+};
 
-export default PagePost
+export default PagePost;
